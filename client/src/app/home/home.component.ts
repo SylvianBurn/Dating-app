4b91df3542feb7635fcee03adb2cc29b0c25cdde
Sylvian Burn <s.burn@technologyandstrategy.com>
2023-06-28 15:29:21 +0200

    end of section 8: members card, photo gallery

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,30 +9,13 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   users: any;
 
-  constructor(
-    private http: HttpClient,
-  ) {
-  }
+  constructor() {}
 
   ngOnInit(): void {
-    this.getUsers();
   }
 
   registerToggle() {
     this.registerMode =!this.registerMode;
-  }
- 
-  getUsers() {
-    this.http.get("https://localhost:5001/api/users").subscribe({
-      next: (users) => {
-        this.users = users;
-        console.log("users: ", users);
-      },
-      error: (error) => {
-        console.error(error);
-      },
-      complete: () => {console.log("request has completed")}
-    });
   }
 
   cancelRegisterMode(event: boolean) {
